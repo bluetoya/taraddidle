0c4b9c90450cf4c024d57e1124283b1bce8e49c6
bluetoya <hjahn934@gmail.com>
2025-02-13 23:51:22 +0900

    feat: initial commit

---
+++ src/main/java/com/bluetoya/taradiddle/TaradiddleApplication.java
@@ -0,0 +1,13 @@
+package com.bluetoya.taradiddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaradiddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaradiddleApplication.class, args);
+	}
+
+}
