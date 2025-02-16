b5e519628208c379bddc4b3306db25c6a6ce0fd5
bluetoya <hjahn934@gmail.com>
2025-02-16 18:28:28 +0900

    feat: 유저 엔티티 추가

---
+++ src/main/java/com/bluetoya/taradiddle/feature/user/User.java
@@ -0,0 +1,27 @@
+package com.bluetoya.taradiddle.feature.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Builder
+@Document(collection = "users")
+public class User {
+
+  @Id private String id;
+  private String username;
+  private String firstName;
+  private String lastName;
+
+  public static User ofMock() {
+    return User.builder()
+        .id("mockId")
+        .username("mockUsername")
+        .firstName("mockFirstName")
+        .lastName("mockLastName")
+        .build();
+  }
+}
