30f0ec8e13b5e64a97f41223faa2b0ff834ea9bf
bluetoya <hjahn934@gmail.com>
2025-02-17 22:41:20 +0900

    feat: 유저 생성하고 6분 뒤에 유저 삭제하도록 createdAt에 TTL index 추가

--- src/main/java/com/bluetoya/taradiddle/feature/user/User.java
+++ src/main/java/com/bluetoya/taradiddle/feature/user/User.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @Document(collection = "users")
@@ -14,18 +16,21 @@ public class User {
   private String username;
   private String firstName;
   private String lastName;
+  private LocalDateTime createdAt;
 
   public static User of(UserDto user) {
     return User.builder()
             .username(user.getUsername())
             .firstName(user.getFirstName())
             .lastName(user.getLastName())
+            .createdAt(LocalDateTime.now())
             .build();
   }
 
   public User update(UserDto user) {
     this.firstName = user.getFirstName();
     this.lastName = user.getLastName();
+    this.createdAt = LocalDateTime.now();
     return this;
   }
 }
