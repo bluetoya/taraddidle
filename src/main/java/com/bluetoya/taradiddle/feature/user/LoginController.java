2e54de57908948c65fe0eef0f80b9053df447ad5
bluetoya <hjahn934@gmail.com>
2025-02-19 01:19:56 +0900

    feat: 타임리프 로그인 화면 설정

---
+++ src/main/java/com/bluetoya/taradiddle/feature/user/LoginController.java
@@ -0,0 +1,19 @@
+package com.bluetoya.taradiddle.feature.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/login")
+public class LoginController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String login() {
+        return "login";
+    }
+}
