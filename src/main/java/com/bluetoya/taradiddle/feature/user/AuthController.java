daba0f531c7da0f8a11fef6d090d32e0b5dc3ffc
bluetoya <hjahn934@gmail.com>
2025-02-20 23:50:59 +0900

    feat: 접근 권한 기능 관점으로 RequestMapping 변경

--- src/main/java/com/bluetoya/taradiddle/feature/user/AuthController.java
+++ src/main/java/com/bluetoya/taradiddle/feature/user/AuthController.java
@@ -1,17 +1,25 @@
 package com.bluetoya.taradiddle.feature.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/login")
-public class LoginController {
+@RequestMapping("/auth")
+public class AuthController {
 
-    @GetMapping
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
+
+    @GetMapping("/sign-in")
+    public String signIn(Model model) {
+    model.addAttribute("authRequest", new AuthRequest());
+        return "signIn";
+    }
+
 }
