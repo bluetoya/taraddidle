ff2463d8aabe7222f59ef34ec4f35a2d624e88eb
bluetoya <hjahn934@gmail.com>
2025-02-22 23:56:18 +0900

    feat: jwt 사용을 위해 filter 추가

---
+++ src/main/java/com/bluetoya/taradiddle/common/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,19 @@
+package com.bluetoya.taradiddle.common.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // check if there is token
+
+    }
+}
