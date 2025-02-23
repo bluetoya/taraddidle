6f8835c58df8f1d6e8c84221a166c32eaf6e803c
bluetoya <hjahn934@gmail.com>
2025-02-23 23:03:10 +0900

    refactor: 불필요한 클래스 삭제

--- src/main/java/com/bluetoya/taradiddle/feature/auth/AuthRequest.java
+++
@@ -1,8 +0,0 @@
-package com.bluetoya.taradiddle.feature.auth;
-
-public record AuthRequest(String username, String password, String passwordConfirmation) {
-
-    public AuthRequest() {
-        this(null, null, null);
-    }
-}
