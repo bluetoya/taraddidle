c87b320f8bfefbb636eb2705620950a1fc28495e
bluetoya <hjahn934@gmail.com>
2025-02-23 23:06:31 +0900

    chore: spring security autoconfiguration 블록 처리

--- src/main/java/com/bluetoya/taradiddle/TaradiddleApplication.java
+++ src/main/java/com/bluetoya/taradiddle/TaradiddleApplication.java
@@ -2,8 +2,9 @@ package com.bluetoya.taradiddle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TaradiddleApplication {
 
 	public static void main(String[] args) {
