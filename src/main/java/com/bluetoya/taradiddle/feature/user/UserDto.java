85ee6cbde6a373520378d2a284166a46ceb97197
bluetoya <hjahn934@gmail.com>
2025-02-23 23:07:08 +0900

    refactor: 유저 내 패스워드 필드 제거 (Auth가 담당)

--- src/main/java/com/bluetoya/taradiddle/feature/user/UserDto.java
+++ src/main/java/com/bluetoya/taradiddle/feature/user/UserDto.java
@@ -1,3 +1,3 @@
 package com.bluetoya.taradiddle.feature.user;
 
-public record UserDto(String username, String password, String firstName, String lastName) {}
+public record UserDto(String username, String firstName, String lastName) {}
