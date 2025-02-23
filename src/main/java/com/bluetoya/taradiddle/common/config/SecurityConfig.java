c01056009854854728af546ccb3d73faa14f6afd
bluetoya <hjahn934@gmail.com>
2025-02-23 14:07:51 +0900

    fix: jwt 인증 전 모든 API 허용되도록 설정

--- src/main/java/com/bluetoya/taradiddle/common/config/SecurityConfig.java
+++ src/main/java/com/bluetoya/taradiddle/common/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.bluetoya.taradiddle.common.config;
 
-import com.bluetoya.taradiddle.common.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,13 +13,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-  private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(
-            auth -> auth.requestMatchers("/").permitAll().anyRequest().authenticated())
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            auth -> auth.anyRequest().permitAll())
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
