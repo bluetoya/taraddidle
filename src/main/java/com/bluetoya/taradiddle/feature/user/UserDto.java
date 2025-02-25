d746f2bcd2dd8c61db322add2f67c1a56dfb2cb7
bluetoya <hjahn934@gmail.com>
2025-02-25 22:51:35 +0900

    refactor: delete unused constructor

--- src/main/java/com/bluetoya/taradiddle/feature/user/UserDto.java
+++ src/main/java/com/bluetoya/taradiddle/feature/user/UserDto.java
@@ -2,8 +2,4 @@ package com.bluetoya.taradiddle.feature.user;
 
 public record UserDto(String username, String firstName, String lastName, String authId) {
 
-    public UserDto(String username, String firstName, String lastName) {
-        this(username, firstName, lastName, null);
-    }
-
 }
