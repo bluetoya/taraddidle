09f876776bfcffb2e59b3ced24db0174836845b3
bluetoya <hjahn934@gmail.com>
2025-02-25 17:24:55 +0900

    feat: RuntimeException을 CustomException으로 변환

--- src/main/java/com/bluetoya/taradiddle/common/exception/errorcode/AuthErrorCode.java
+++ src/main/java/com/bluetoya/taradiddle/common/exception/errorcode/AuthErrorCode.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효한 토큰이 아닙니다."),
-
     USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 아이디입니다."),
     WRONG_PASSWORD(HttpStatus.NOT_FOUND, "틀린 패스워드 입니다."),
     UNMATCHED_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 다릅니다."),
--- src/main/java/com/bluetoya/taradiddle/feature/auth/AuthServiceImpl.java
+++ src/main/java/com/bluetoya/taradiddle/feature/auth/AuthServiceImpl.java
@@ -1,6 +1,8 @@
 package com.bluetoya.taradiddle.feature.auth;
 
 import com.bluetoya.taradiddle.common.config.JwtProvider;
+import com.bluetoya.taradiddle.common.exception.CustomException;
+import com.bluetoya.taradiddle.common.exception.errorcode.AuthErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,17 +19,16 @@ public class AuthServiceImpl implements AuthService {
 
     private final AuthRepository authRepository;
 
-    // TODO :: exception 관리 클래스 생성 필요
     @Override
     public LoginResponse login(String userId, String password) {
         AuthUser authUser = authRepository.findByUserId(userId);
 
         if (!isUserExists(authUser)) {
-            throw new RuntimeException("존재하지 않는 회원입니다.");
+            throw new CustomException(AuthErrorCode.USER_ID_NOT_FOUND);
         }
 
         if (!bCryptPasswordEncoder.matches(password, authUser.getPassword())) {
-            throw new RuntimeException("틀린 패스워드를 입력했습니다.");
+            throw new CustomException(AuthErrorCode.WRONG_PASSWORD);
         }
 
         return new LoginResponse(jwtProvider.generateAccessToken(userId));
@@ -39,14 +40,19 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public SignInResponse signIn(SignInRequest request) {
+        // TODO :: 입력한 아이디 유효성 검사
+        // TODO :: 비밀번호 길이나 조합 내역 검사
+
         if (!isPasswordCorrect(request)) {
-            throw new RuntimeException("입력하신 패스워드와 확인용 패스워드가 다릅니다.");
+            throw new CustomException(AuthErrorCode.UNMATCHED_PASSWORD);
         }
 
         String encryptedPassword = bCryptPasswordEncoder.encode(request.password());
 
-        // TODO :: 로그인 시 필요한 validation 추가
         AuthUser authUser = authRepository.save(AuthUser.of(request.userId(), encryptedPassword));
+        // TODO :: authUser 생성 후 user도 생성해서 키로 매핑
+
+        // TODO :: authUser와 user 내용도 같이 반환
         return new SignInResponse("ok");
     }
 
