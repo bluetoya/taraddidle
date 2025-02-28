d6fa7fc88b8c3a04ee0395d2ce1174d2c0d92955
bluetoya <hjahn934@gmail.com>
2025-03-01 01:28:51 +0900

    fix: regex 변경

--- src/main/java/com/bluetoya/taradiddle/common/util/RegexUtil.java
+++ src/main/java/com/bluetoya/taradiddle/common/util/RegexUtil.java
@@ -5,9 +5,9 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class RegexUtil {
-    private static final Pattern UPPER_CASE_PATTERN = Pattern.compile("[A-Z]");
-    private static final Pattern LOWER_CASE_PATTERN = Pattern.compile("[a-z]");
-    private static final Pattern NUMBER_PATTERN = Pattern.compile("[0-9]");
+    private static final Pattern UPPER_CASE_PATTERN = Pattern.compile(".*[A-Z].*");
+    private static final Pattern LOWER_CASE_PATTERN = Pattern.compile(".*[a-z].*");
+    private static final Pattern NUMBER_PATTERN = Pattern.compile(".*\\d.*");
 
     private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
