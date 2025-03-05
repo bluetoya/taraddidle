f210e3ff5bd88b81461f96d9323bb27fc0d29ff5
bluetoya <hjahn934@gmail.com>
2025-03-06 00:05:02 +0900

    fix: 순환 참조 제거를 위해 filter가 아닌 서비스 레이어에서 로그인하도록 원복

--- src/main/java/com/bluetoya/taradiddle/common/security/SecurityConfig.java
+++ src/main/java/com/bluetoya/taradiddle/common/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.bluetoya.taradiddle.common.security;
 
-import com.bluetoya.taradiddle.common.filter.JwtAuthenticationFilter;
 import com.bluetoya.taradiddle.common.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
-@Component
 @EnableWebSecurity
+@Component
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthorizationFilter jwtAuthorizationFilter;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -37,8 +35,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(
                 session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(jwtAuthorizationFilter, JwtAuthorizationFilter.class);
+            .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- src/main/java/com/bluetoya/taradiddle/feature/auth/controller/AuthController.java
+++ src/main/java/com/bluetoya/taradiddle/feature/auth/controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.bluetoya.taradiddle.feature.auth.controller;
 
+import com.bluetoya.taradiddle.feature.auth.dto.LoginRequest;
 import com.bluetoya.taradiddle.feature.auth.dto.SignInRequest;
 import com.bluetoya.taradiddle.feature.auth.dto.SignInResponse;
 import com.bluetoya.taradiddle.feature.auth.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,11 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthenticationService authenticationService;
+
+    @GetMapping("/login")
+    public void login(final @RequestBody LoginRequest request, HttpServletResponse response) {
+        authenticationService.login(request, response);
+    }
 
     @PostMapping("/sign-in")
     public SignInResponse signIn(final @RequestBody SignInRequest request) {
