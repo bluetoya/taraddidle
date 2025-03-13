35826f0f1d062d9ce76bd9c459c5f6b0a980530e
bluetoya <hjahn934@gmail.com>
2025-03-13 23:18:05 +0900

    refactor: delete unused method

--- src/main/java/com/bluetoya/taradiddle/feature/user/repository/UserRepository.java
+++ src/main/java/com/bluetoya/taradiddle/feature/user/repository/UserRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String>, UserCustomRepository {
 
-    void deleteByUsername(String username);
-
     Optional<User> findByEmail(String email);
 }
