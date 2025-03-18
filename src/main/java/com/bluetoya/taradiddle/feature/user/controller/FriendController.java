60973b132c2069820bc63f190081a2d9fac44dcf
bluetoya <hjahn934@gmail.com>
2025-03-18 23:41:34 +0900

    refactor: 친구 API comment out 변경, null로 일단 수정

--- src/main/java/com/bluetoya/taradiddle/feature/user/controller/FriendController.java
+++ src/main/java/com/bluetoya/taradiddle/feature/user/controller/FriendController.java
@@ -1,5 +1,8 @@
 package com.bluetoya.taradiddle.feature.user.controller;
 
+import com.bluetoya.taradiddle.common.ApiResponse;
+import com.bluetoya.taradiddle.feature.user.dto.FriendDto;
+import com.bluetoya.taradiddle.feature.user.entity.User;
 import com.bluetoya.taradiddle.feature.user.service.FriendService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,14 +14,14 @@ public class FriendController {
 
     private final FriendService friendService;
 
-//    @PatchMapping("/{username}")
-//    public ApiResponse<User> addFriend(@PathVariable String username, @RequestBody FriendDto friend) {
-//        return new ApiResponse<>(friendService.addFriend(username, friend));
-//    }
-//
-//    @DeleteMapping("/{username}")
-//    public ApiResponse<User> remove(@PathVariable String username, @RequestBody FriendDto friend) {
-//        return new ApiResponse<>(friendService.removeFriend(username, friend));
-//    }
+    @PatchMapping("/{username}")
+    public ApiResponse<User> addFriend(@PathVariable String username, @RequestBody FriendDto friend) {
+        return new ApiResponse<>(null);
+    }
+
+    @DeleteMapping("/{username}")
+    public ApiResponse<User> remove(@PathVariable String username, @RequestBody FriendDto friend) {
+        return new ApiResponse<>(null);
+    }
 
 }
